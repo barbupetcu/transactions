ab0b99ed676530c8b7ba1647432977d3b61143bf
Petcu Barbu <42030194+barbupetcu@users.noreply.github.com>
2019-08-29 17:02:11 +0300

    add cnp validator

--- validation/src/main/java/com/transactions/validation/validators/CnpValidator.java
+++ validation/src/main/java/com/transactions/validation/validators/CnpValidator.java
@@ -22,7 +22,25 @@ public class CnpValidator implements ConstraintValidator<CnpConstraint, String> 
 
     //todo completat metoda de validare a cnpului
     private boolean checkCnp(String cnp){
-        return true;
+        int[] cnpArray = new int[13];
+
+        for(int i=0; i<cnp.length(); i++){
+            cnpArray[i] = Integer.parseInt(String.valueOf(cnp.charAt(i)));
+        }
+
+        if(cnp.length() == 13){
+
+            long sum = cnpArray[0] * 2 + cnpArray[1] * 7 + cnpArray[2] * 9 + cnpArray[3] * 1 + cnpArray[4] * 4 + cnpArray[5] * 6
+                    + cnpArray[6] * 3 + cnpArray[7] * 5 + cnpArray[8] * 8 + cnpArray[9] * 2 + cnpArray[10] * 7 + cnpArray[11] * 9;
+
+            long rest = sum % 11;
+
+            if(((rest < 10) && (rest == cnpArray[12])) || ((rest == 10) && (cnpArray[12] == 1))){
+                return true;
+            }
+        }
+        
+        return false;
     }
 
 
