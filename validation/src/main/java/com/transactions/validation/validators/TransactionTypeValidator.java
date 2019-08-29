eb3d00187053c1a81fabe2abb099b8d478ca2c8a
Petcu Barbu <42030194+barbupetcu@users.noreply.github.com>
2019-08-29 17:27:01 +0300

    Update TransactionTypeValidator.java

--- validation/src/main/java/com/transactions/validation/validators/TransactionTypeValidator.java
+++ validation/src/main/java/com/transactions/validation/validators/TransactionTypeValidator.java
@@ -1,1 +1,27 @@
 
+package com.transactions.validation.validators;
+
+import org.apache.commons.validator.routines.checkdigit.IBANCheckDigit;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class TransactionTypeValidator implements ConstraintValidator<TransactionTypeConstraint, String> {
+  
+  private final static List<String> transactionsTypes = Arrays.asList("IBAN_TO_IBAN","IBAN_TO_WALLET","WALLET_TO_IBAN","WALLET_TO_WALLET");
+
+    @Override
+    public void initialize(TransactionTypeConstraint TransactionTypeConstraint) {
+    }
+
+    /*
+    * Metoda in care se face validarea, daca aceasta returneaza false atunci
+    *  este aruncata exceptia MethodArgumentNotValidException cu mesajul din interfata TransactionTypeConstraint
+     */
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        return transactionsTypes.contains(value);
+    }
+
+
+}
