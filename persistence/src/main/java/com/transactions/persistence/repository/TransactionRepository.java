ed41fdf91849dfabf3b1ebab386595006f4c1036
Petcu Barbu <42030194+barbupetcu@users.noreply.github.com>
2019-08-30 18:31:46 +0300

    Update TransactionRepository.java

--- persistence/src/main/java/com/transactions/persistence/repository/TransactionRepository.java
+++ persistence/src/main/java/com/transactions/persistence/repository/TransactionRepository.java
@@ -1,8 +1,24 @@
 package com.transactions.persistence.repository;
 
 import com.transactions.persistence.model.Transaction;
+import com.transactions.persistence.model.projection.TransactionDetailsDto;
+import com.transactions.persistence.model.projection.TransactionTypeDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("select new TransactionTypeDto(t.type, count(t), sum(t.amount)) from Transaction t where t.payer.cnp = :cnp group by t.type")
+    List<TransactionTypeDto> getAggTransactionsByUser (@Param("cnp") String cnp);
+
+    @Query("select new TransactionDetailsDto(t.description, t.amount, t.payer.name, t.payer.cnp, t.payer.iban)" +
+            " from Transaction t where t.type = :type and t.payer.cnp = :cnp")
+    List<TransactionDetailsDto> getDetailedTranByTypeAndCnp(@Param("type") String type, @Param("cnp") String cnp);
+
+
+
 
 }
