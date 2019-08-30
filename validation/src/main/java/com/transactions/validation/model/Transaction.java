6223f5d0817c5b009e33a7dfd5f6f6d6eae465fb
Petcu Barbu <42030194+barbupetcu@users.noreply.github.com>
2019-08-30 14:50:17 +0300

    Update Transaction.java

--- validation/src/main/java/com/transactions/validation/model/Transaction.java
+++ validation/src/main/java/com/transactions/validation/model/Transaction.java
@@ -1,6 +1,7 @@
 package com.transactions.validation.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.transactions.validation.validators.Conditional;
 import com.transactions.validation.validators.TransactionTypeContraint;
 
 import javax.validation.Valid;
@@ -8,24 +9,49 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
+@Conditional(selected = "type", values = {"IBAN_TO_IBAN", "IBAN_TO_WALLET"}, required = {"payer.iban"})
+@Conditional(selected = "type", values = {"IBAN_TO_IBAN", "WALLET_TO_IBAN"}, required = {"payee.iban"} )
 public class Transaction {
 
     @NotBlank(message = "Tipul tranzactiei este obligatoriu")
     @TransactionTypeContraint
     @JsonProperty
     private String type;
+
     @NotBlank(message = "Descrierea este obligatorie")
     @JsonProperty
     private String description;
+
     @DecimalMin(value = "0.01", message = "Suma minima transactionata este 0.01")
     @JsonProperty
     private BigDecimal amount;
+
     @Valid
     @JsonProperty
     private TransactionActor payer;
+
     @Valid
     @JsonProperty
     private TransactionActor payee;
+
+    public static enum TransactionType {
+
+        IBAN_TO_IBAN("IBAN_TO_IBAN"),
+        IBAN_TO_WALLET("IBAN_TO_WALLET"),
+        WALLET_TO_IBAN("WALLET_TO_IBAN"),
+        WALLET_TO_WALLET("WALLET_TO_WALLET");
+
+        private String type;
+
+        TransactionType(String type){
+            this.type=type;
+        }
+
+        @Override
+        public String toString() {
+            return type;
+        }
+    }
 
     public String getType() {
         return type;
