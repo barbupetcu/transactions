d5470072d7d5df6bed694a975e5f6dd63fd5cc87
Petcu Barbu <42030194+barbupetcu@users.noreply.github.com>
2019-08-30 14:49:09 +0300

    Update ValidationController.java

--- validation/src/main/java/com/transactions/validation/controller/ValidationController.java
+++ validation/src/main/java/com/transactions/validation/controller/ValidationController.java
@@ -1,40 +1,37 @@
 package com.transactions.validation.controller;
 
-import com.transactions.validation.model.Transaction;
-import com.transactions.validation.producer.TransactionSender;
-import com.transactions.validation.service.ValidationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.validation.Valid;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-@RestController
-@RequestMapping("/")
-public class ValidationController {
+@ControllerAdvice
+public class RestExceptionHandler {
 
-    @Autowired
-    ValidationService validationService;
-
-    //Validarea campurilor se face cu ajutorul adnotarilor de pe fiecare atribut in parte
-    @PostMapping("createTransaction")
-    public ResponseEntity<Map<String, Object>> createTransaction(@Valid @RequestBody Transaction transaction) throws MethodArgumentNotValidException {
-
-        //Daca datele tranzactiei sunt validate atunci aceastea sunt trimise in coada de mesaje Kafka
-        validationService.createTransaction(transaction);
-
+    /*
+    De fiecare data cand un camp nu este validat atunci este aruncata eroarea MethodArgumentNotValidException.
+    Cand se arunca aceasta eroare atunci serviciile rest vor returna un map cu numele campului + mesajul de eroare
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", new Date());
-        body.put("message", "Solicitarea a fost inregistrata");
 
-        return ResponseEntity.ok(body);
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        body.put("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
-
 }
